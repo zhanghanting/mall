5ac742ab8ee82954ba7a6e58cd71ddf238ced36f
TianMingForJava <sunbw@163.com>
2019-05-24 15:10:17 +0800

    a

--- src/main/java/com/springboot/mall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/springboot/mall/service/impl/CategoryServiceImpl.java
@@ -1,19 +1,33 @@
 package com.springboot.mall.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.springboot.mall.domain.Category;
 import com.springboot.mall.mapper.CategoryMapper;
 import com.springboot.mall.service.CategoryService;
 import com.springboot.mall.utils.JsonBean;
+import com.springboot.mall.utils.MallPageHelper;
 import com.springboot.mall.utils.PageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
     @Autowired
     CategoryMapper categoryMapper;
+
     @Override
     public JsonBean viewAllCategory(PageUtils pageUtils) {
-         /*List<Category> categoryMapper.viewAllCategory()*/
-        return null;
+        /*PageHelper.startPage(pageUtils.getPage(),pageUtils.getLimit());
+        List<Category> categories = categoryMapper.viewAllCategory();
+        PageInfo pageInfo = new PageInfo(categories);
+        MallPageHelper mallPageHelper = new MallPageHelper();
+        mallPageHelper.setItems(categories);
+        mallPageHelper.setTotal(pageInfo.getTotal());*/
+        List<Category> categories = categoryMapper.viewAllCategory();
+        return new JsonBean(categories,0,"OK");
     }
 }
